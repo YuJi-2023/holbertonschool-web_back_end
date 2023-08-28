aa6ba11835e0444c5b16821a3b4bb602c3d43137
YuJi-2023 <sophia.jiyu@gmail.com>
2023-08-28 00:50:21 +0000

    task3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""type-annotated function to_string"""
+
+
+def to_str(n: float) -> str:
+    """to_string function takes a float n and return string representation"""
+    result = str(n)
+    return result
