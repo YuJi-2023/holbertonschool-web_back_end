c32671197cd826a8d64c93bcd9d7d03e860e05a0
YuJi-2023 <sophia.jiyu@gmail.com>
2023-08-28 00:32:41 +0000

    task 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""type-annotated function"""
+
+
+def add(a: float, b: float) -> float:
+    """add function takes a and b and returns their sum"""
+    result = a + b
+    return result
