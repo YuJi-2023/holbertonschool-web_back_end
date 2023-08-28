1d3551b15477a8ddb15ce7e79970fe9cdbe08d13
YuJi-2023 <sophia.jiyu@gmail.com>
2023-08-28 01:35:35 +0000

    update task7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,12 +3,10 @@
 from typing import Tuple
 from typing import Union
 
-v = Union[int, float]
 
-
-def to_kv(k: str, v: float) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """function takes string k and mixed type v, return as a tuple"""
-    first = k
-    second = v ** 2
+    first: str = k
+    second: float = v ** 2
     result = (first, second)
     return result
