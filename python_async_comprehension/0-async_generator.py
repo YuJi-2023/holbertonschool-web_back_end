82ec6a197441e1919dc5d22fca15633038b04c80
YuJi-2023 <sophia.jiyu@gmail.com>
2023-08-28 12:21:57 +0000

    update type annotation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """create a coroutine async_generator"""
 import random
 import asyncio
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """the coroutine loop 10 times and wait 1 sec, then random out a number"""
     for i in range(10):
         await asyncio.sleep(1)
