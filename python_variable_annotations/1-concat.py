9bc6ac3161bd84bedcd71561d6a7c696576946f8
YuJi-2023 <sophia.jiyu@gmail.com>
2023-08-28 00:37:07 +0000

    task1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""type-annotated function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concat function takes two strings and concat them and return"""
+    result = str1 + str2
+    return result
