ca70e573932bba69771b8975e46b8b86cd916e35
YuJi-2023 <sophia.jiyu@gmail.com>
2023-08-28 00:42:45 +0000

    task2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""type-annotated function floor"""
+import math
+
+
+def floor(n: float) -> int:
+    """floor function takes a float n and return its floor"""
+    result = math.floor(n)
+    return result
