9cf4dffc447772cd41072595285e142ad7cba1f1
YuJi-2023 <sophia.jiyu@gmail.com>
2023-08-28 11:52:08 +0000

    task4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""an asynchronous coroutine wait_n"""
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """takes two ints and return the list of all delays ascendingly"""
+    result_list = []
+    for count in range(n):
+        delay = await task_wait_random(max_delay)
+        result_list.append(delay)
+    for i in range(len(result_list)):
+        for j in range(len(result_list) - i - 1):
+            if result_list[j] > result_list[j + 1]:
+                result_list[j], result_list[j + 1] =\
+                        result_list[j + 1], result_list[j]
+    return result_list
