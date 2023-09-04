d9102ab3f700f6d0a42c6fb914d7f059c873763f
YuJi-2023 <sophia.jiyu@gmail.com>
2023-09-03 22:05:54 -0700

    task 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""using pyMongo"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """a function changes all topics of a school document based on the name"""
+    my_query = {"name": name}
+    new_topics = {"$set": {"topics": topics}}
+    mongo_collection.update_one(my_query, new_topics)
