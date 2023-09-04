db9bcc2d8e3523cb9ab3190594d621f036f08af1
YuJi-2023 <sophia.jiyu@gmail.com>
2023-09-03 23:21:50 -0700

    update_many

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -6,4 +6,4 @@ def update_topics(mongo_collection, name, topics):
     """a function changes all topics of a school document based on the name"""
     my_query = {"name": name}
     new_topics = {"$set": {"topics": topics}}
-    mongo_collection.update(my_query, new_topics, {multi: true})
+    mongo_collection.update_many(my_query, new_topics)
