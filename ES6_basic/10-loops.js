8727baf5e0164907c75e66562d577602be442d65
Yu Ji <sophia.jiyu@gmai.com>
2023-09-13 11:10:18 +0000

    update task10 use const instead of let

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
-  let elem;
-  for (elem of array) {
+  for (const elem of array) {
     newArray.push(appendString + elem);
   }
 
