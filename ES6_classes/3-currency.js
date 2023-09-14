97b5f0f43a5024bc5d0bf3f33f3e426faf8b2c5c
Yu Ji <sophia.jiyu@gmai.com>
2023-09-14 03:38:27 +0000

    task3 currenty class with getter setter

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,32 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw TypeError;
+    }
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw TypeError;
+    }
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
