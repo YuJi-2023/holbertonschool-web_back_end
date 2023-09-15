6bc5c63fbb1a967490c8db83d444b603ae6b37df
Yu Ji <sophia.jiyu@gmai.com>
2023-09-15 00:56:12 +0000

    update task2 setter

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,40 +1,52 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.setName(name);
+    this.setLength(length);
+    this.setStudents(students);
   }
 
   get name() {
     return this._name;
   }
 
-  set name(name) {
+  setName(name) {
     if (typeof name !== 'string') {
       throw TypeError('Name must be a string');
     }
     this._name = name;
   }
 
+  set name(newName) {
+    this.setName(newName);
+  }
+
   get length() {
     return this._length;
   }
 
-  set length(length) {
+  setLength(length) {
     if (typeof length !== 'number') {
       throw TypeError('Length must be a number');
     }
     this._length = length;
   }
 
+  set length(newLength) {
+    this.setLength(newLength);
+  }
+
   get students() {
     return this._students;
   }
 
-  set students(students) {
+  setStudents(students) {
     if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
       throw TypeError('Students must be an array of strings');
     }
     this._students = students;
+  }
+
+  set students(newStudents) {
+    this.setStudents(newStudents);
   }
 }
