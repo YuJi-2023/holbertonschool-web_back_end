27972165b939b42aaeb7d6a0ed477d67b7571dc4
Yu Ji <sophia.jiyu@gmai.com>
2023-10-03 01:14:24 +1100

    update task2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,18 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  const myPromise = new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({
-        status: 200,
-        body: 'success',
-      });
-    } else {
-      reject(new Error());
-    }
-  });
-  myPromise
-    .then((response) => {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => {
       console.log('Got a response from the API');
-      return response;
     });
-  return myPromise;
 }
