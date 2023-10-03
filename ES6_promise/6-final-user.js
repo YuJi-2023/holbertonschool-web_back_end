6d33f3d123e64dc17ff5ac2cf4f4e181123c7441
Yu Ji <sophia.jiyu@gmai.com>
2023-10-03 14:47:29 +1100

    change back

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,12 +5,8 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
 
-  // return Promise.all([promise1, promise2].map((p) => p.then((result) => ({
-  //   status: 'fulfilled',
-  //   value: result,
-  // })).catch((e) => ({ status: 'rejected', value: `Error: ${e.message}` }))));
-  return Promise.allSettled([promise1, promise2]).then((results) => results.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  })));
+  return Promise.all([promise1, promise2].map((p) => p.then((result) => ({
+    status: 'fulfilled',
+    value: result,
+  })).catch((e) => ({ status: 'rejected', value: `Error: ${e.message}` }))));
 }
